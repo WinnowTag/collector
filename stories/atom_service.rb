6b773a95b06186e9dbcdfd186042cf3cbfdc72a4
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-01-21 03:56:19 +0000

    Restructured stories a bit.  Fleshed out feed accessing scenario.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/collector@2674 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- stories/atom_service.rb
+++ stories/atom_service.rb
@@ -1,76 +1,93 @@
 require File.dirname(__FILE__) + '/helper'
 require File.dirname(__FILE__) + '/server'
 
-Story "Atom Client Access", %{
-  As another system
-  I want to be able to access feeds and items using atom
-  So that I can cache feeds and items in my own format
-} do
+steps_for(:atom_service_interation) do
+  # Givens
+  Given("existing feeds in the system") do 
+    @feeds = Feed.find(:all)
+  end
   
-  Scenario "Accessing the service description" do
-    Given "existing feeds in the system", 0 do 
-      @feeds = Feed.find(:all)
-    end
-    
-    And  "a base url" do
-      @base = 'http://localhost:4000'
-    end
-    
-    When "I fetch the service document" do
-      @service = Atom::Pub::Service.load_service(URI.parse("#{@base}/service"))
-    end
-    
-    Then "the document should have a workspace" do
-      @service.should have(1).workspaces
-    end
-    
-    And  "the workspace should have a title" do
-      @service.workspaces.first.title.should_not be_nil
-    end
-    
-    And  "the workspace should have a collection for each feed" do
-      @service.workspaces.first.collections.size.should == @feeds.size
-    end
-    
-    And  "the collections should have hrefs pointing to the feeds" do
-      @feeds.each_with_index do |feed, index|
-        @service.workspaces.first.collections[index].href.should == "#{@base}/feeds/#{feed.id}.atom"
-      end
-    end
-    
-    And  "the collections should have titles for each of the feeds" do
-      @feeds.each_with_index do |feed, index|
-        @service.workspaces.first.collections[index].title.should == feed.title
-      end
+  Given("feed $id in the system") do |id|
+    @feed = Feed.find(id)
+  end
+  
+  Given("item $i of the feed") do |i|
+    @item = @feed.feed_items[i.to_i - 1]
+  end
+  
+  Given("a base url") do
+    @base = 'http://localhost:4000'
+  end
+  
+  # Whens
+  When("I fetch the service document") do
+    @service = Atom::Pub::Service.load_service(URI.parse("#{@base}/service"))
+  end
+  
+  When("I fetch the feed for the first collection") do
+    @fetched_feed = @service.workspaces.first.collections.first.feed
+  end
+  
+  When("item $i of the feed") do |i|
+    @item = @fetched_feed.entries[i.to_i - 1]
+  end
+  
+  # Thens
+  Then("the document should have a workspace") do
+    @service.should have(1).workspaces
+  end
+  
+  Then("the workspace should have a title") do
+    @service.workspaces.first.title.should_not be_nil
+  end
+  
+  Then("the workspace should have a collection for each feed") do
+    @service.workspaces.first.collections.size.should == @feeds.size
+  end
+  
+  Then("the collections should have hrefs pointing to the feeds") do
+    @feeds.each_with_index do |feed, index|
+      @service.workspaces.first.collections[index].href.should == "#{@base}/feeds/#{feed.id}.atom"
     end
   end
   
-  Scenario "Accessing a feed" do
-    Given "existing feeds in the system"
-    And "a base url"
-    
-    When "I fetch the service document"
-    And  "fetch the feed for the first collection"
-    
-    Then "the feed contains the items"
-    And  "the $link matches self", :first
-    And  "the $link matches self", :last
-    And  "the $link is missing", :prev
-    And  "the $link is missing", :next
-    And  "the id fragment matches the system id"
+  Then("the collections should have titles for each of the feeds") do
+    @feeds.each_with_index do |feed, index|
+      @service.workspaces.first.collections[index].title.should == feed.title
+    end
   end
   
-  Scenario "Accessing an item for a feed" do
-    Given "existing feeds in the system"
-    And "a base url"
-    And "the first item of the first feed"
-    
-    When "I fetch the service document"
-    And "fetch the feed for the first collection"
-    
-    Then "then the first item matches the first item of the first feed"
-    And  "the item's alternate link points to the atom representation for the item"
-    And  "the item's spider link points to the spider URL for the item"
-    And  "the id fragment matches the system id"
+  Then("the feed contains the title") do
+    @fetched_feed.title.should == @feed.title
   end
+  
+  Then("the feed contains the items") do
+    @fetched_feed.entries.size.should == @feed.feed_items.size
+  end
+  
+  Then("the feed self link points to self") do
+    @fetched_feed.links.self.href.should == "#{@base}/feeds/#{@feed.id}.atom"
+  end
+  
+  Then("the feed alternate link points to source html") do
+    @fetched_feed.links.alternate.href.should == @feed.link
+  end
+  
+  Then("the $link link matches $link") do |link1_rel, link2_rel|
+    link2 = @fetched_feed.links.detect {|link| link.rel == link2_rel }
+    link2.should_not be_nil
+    @fetched_feed.links.detect {|link| link.rel == link1_rel}.should == link2
+  end
+  
+  Then("the $link link is missing") do |link_rel|
+    @fetched_feed.links.detect{|l| l.rel == link_rel}.should be_nil
+  end
+  
+  Then("the id fragment matches the feed id") do
+    URI.parse(@fetched_feed.id).fragment.to_i.should == @feed.id
+  end
+end
+
+with_steps_for(:atom_service_interation) do
+  run_local_story "atom_service"
 end
