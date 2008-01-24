fd0a62b37cb35f745df690b44c046b533aba272b
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-01-24 02:43:53 +0000

    Added 'No Peeping Tom' plugin to disable observers during testing.  Prevents atom publishing from be triggered unless we are specifically testing it.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/collector@2708 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- spec/models/item_cache_observer_spec.rb
+++ spec/models/item_cache_observer_spec.rb
@@ -10,40 +10,54 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ItemCacheObserver do  
   fixtures :item_caches
   it "should publish a feed when it is created" do
-    feed = Feed.new(:url => 'http://example.com')
-    ItemCache.should_receive(:publish).with(feed)
-    feed.save!
+    Feed.with_observers(:item_cache_observer) do
+      feed = Feed.new(:url => 'http://example.com')
+      ItemCache.should_receive(:publish).with(feed)
+      feed.save!
+    end
   end
   
   it "should publish a feed item when it is created" do
-    feed = Feed.find(1)
-    fi = feed.feed_items.build(:link => 'http://example.com/test', :title => 'test', :unique_id => 'test')
-    ItemCache.should_receive(:publish).with(fi)
-    fi.save!
+    FeedItem.with_observers(:item_cache_observer) do
+      feed = Feed.find(1)
+      fi = feed.feed_items.build(:link => 'http://example.com/test', :title => 'test', :unique_id => 'test')
+      ItemCache.should_receive(:publish).with(fi)
+      fi.save!
+    end
   end
   
   it "should update ItemCaches when a feed is saved" do
-    feed = Feed.find(1)
-    ItemCache.should_receive(:update).with(feed)
-    feed.save!
+    Feed.with_observers(:item_cache_observer) do
+      feed = Feed.find(1)
+      ItemCache.should_receive(:update).with(feed)
+      feed.save!
+    end
   end
   
   it "should update ItemCaches when a feed item is saved" do
-    fi = FeedItem.find(1)
-    ItemCache.should_receive(:update).with(fi)
-    fi.save!
+    FeedItem.with_observers(:item_cache_observer) do
+      fi = FeedItem.find(1)
+      ItemCache.should_receive(:update).with(fi)
+      fi.save!
+    end
   end
   
   it "should delete a feed from ItemCaches when it is destroyed" do
-    feed = Feed.find(1)
-    ItemCache.should_receive(:delete).with(feed)
-    ItemCache.should_receive(:delete).with(an_instance_of(FeedItem)).never
-    feed.destroy
+    Feed.with_observers(:item_cache_observer) do
+      FeedItem.with_observers(:item_cache_observer) do
+        feed = Feed.find(1)
+        ItemCache.should_receive(:delete).with(feed)
+        ItemCache.should_receive(:delete).with(an_instance_of(FeedItem)).never
+        feed.destroy
+      end
+    end
   end
   
   it "should delete a feed item from ItemCaches when it is destroyed" do
-    fi = FeedItem.find(1)
-    ItemCache.should_receive(:delete).with(fi)
-    fi.destroy
+    FeedItem.with_observers(:item_cache_observer) do
+      fi = FeedItem.find(1)
+      ItemCache.should_receive(:delete).with(fi)
+      fi.destroy
+    end
   end
 end
