663f2a166e73010929837d18e9024420877f3c06
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-08 02:01:24 +0000

    Added missing created_on attributes to feeds. Fixes #598.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/collector@2866 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

---
+++ db/migrate/032_add_missing_created_on_dates_to_feeds.rb
@@ -0,0 +1,20 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+
+class AddMissingCreatedOnDatesToFeeds < ActiveRecord::Migration
+  def self.up
+    execute "update feeds set created_on = " +
+              "(select created_on from feed_items " +
+                "where feed_id = feeds.id "+
+                "order by created_on asc limit 1) " +
+              "where created_on is null;"
+    execute "update feeds set created_on = updated_on where created_on is null;"
+  end
+
+  def self.down
+  end
+end
