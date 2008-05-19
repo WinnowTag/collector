850d6154b4f84e6eb45c60bd0f689917ba8c4d1c
Sean Geoghegan <seangeo@gmail.com>
2008-05-19 12:45:16 +0930

    Turn off rails based log rotation for spidering. See #811.

--- config/initializers/spider.rb
+++ config/initializers/spider.rb
@@ -1,4 +1,4 @@
 Dependencies.load_once_paths << File.join(RAILS_ROOT, 'lib', 'spider.rb')
 Dependencies.load_once_paths << File.join(RAILS_ROOT, 'lib', 'scrapers')
-Spider.logger = Logger.new(File.join(RAILS_ROOT, 'log', 'spider.log'), 'weekly')
+Spider.logger = Logger.new(File.join(RAILS_ROOT, 'log', 'spider.log'))
 Spider.load_scrapers(File.join(RAILS_ROOT, 'lib', 'scrapers'))
