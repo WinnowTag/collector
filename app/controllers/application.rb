104f68b0cb455712212744f6f0730f4551c8f7c5
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-07-14 15:59:26 -0400

    Update application.rb to rails 2.1

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,12 +3,18 @@
 # Possession of a copy of this file grants no permission or license
 # to use, modify, or create derivate works.
 # Please contact info@peerworks.org for further information.
-#
-
-# Filters added to this controller will be run for all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
+  # helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  # protect_from_forgery # :secret => '080aa774f53be2c661d1f81457c4ee46'
+
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  filter_parameter_logging :password
+  
   include ExceptionNotifiable
   include AuthenticatedSystem
   before_filter :login_from_cookie, :login_required, :flash_completed_collections
@@ -26,12 +32,12 @@ class ApplicationController < ActionController::Base
         :status => 400
       } unless defined?(MISSING_PARAMS)
       
-  protected
+protected
   def local_request?
     ["216.176.189.36", "216.176.191.98", "127.0.0.1"].include?(request.remote_ip)
   end
   
-  private
+private
   def flash_completed_collections
     if current_user and params[:format] != 'atom'
       jobs = CollectionJob.completed_jobs_for_user(current_user.login)
