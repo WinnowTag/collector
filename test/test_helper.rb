9e5c268937d436360d27d5682b75821d2e62e9ff
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-07-14 15:57:05 -0400

    Updated test_helper to rails 2.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,4 @@
 ENV["RAILS_ENV"] = "test"
-require 'test/unit'
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
@@ -17,6 +16,10 @@ class Test::Unit::TestCase
   # in MySQL.  Turn off transactional fixtures in this case; however, if you
   # don't care one way or the other, switching from MyISAM to InnoDB tables
   # is recommended.
+  #
+  # The only drawback to using transactional fixtures is when you actually 
+  # need to test transactions.  Since your test is bracketed by a transaction,
+  # any transactions started in your code will be automatically rolled back.
   self.use_transactional_fixtures = true
 
   # Instantiated fixtures are slow, but give you @david where otherwise you
@@ -25,6 +28,12 @@ class Test::Unit::TestCase
   # instantiated fixtures translates to a database query per test method),
   # then set this back to true.
   self.use_instantiated_fixtures  = false
+
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
   def assert_valid(o, msg = "The object should be valid")
