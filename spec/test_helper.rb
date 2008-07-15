5a623b2abf47c72bfef1c86bdcb5bc7c8132ae7a
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-07-15 10:17:56 -0400

    Remove helper testcase

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] = "test"
 require 'test/unit'
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-require File.expand_path(File.dirname(__FILE__) + '/helper_testcase')
 
 class Test::Unit::TestCase
   include AuthenticatedTestHelper
