b10aa83fabe932832550796513d3f8d2fa0bc474
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-07-15 10:20:52 -0400

    Fix some specs. Revert archival history model

--- spec/views/feeds/index.rhtml_spec.rb
+++ spec/views/feeds/index.rhtml_spec.rb
@@ -9,7 +9,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe '/feeds/index.rhtml' do
   it "should escape titles in the confirm dialog for deletion" do
-    assigns[:feeds] = [mock_model(Feed, valid_feed_attributes(:title => "quote&apos;s"))]
+    assigns[:feeds] = [mock_model(Feed, valid_feed_attributes(:title => "quote&apos;s"))].paginate
     render '/feeds/index.rhtml'
     
     response.should have_tag("a[onclick*='confirm('Do you really want to delete quote\\'s?')']", true, response.body)
