1954e48fcbe56cd58fb3f90210b55fce5a267b76
Sean Geoghegan <seangeo@gmail.com>
2008-08-05 13:44:29 +0930

    Remove item protection and archival

--- db/migrate/20080805040437_remove_item_protection.rb
+++ db/migrate/20080805040437_remove_item_protection.rb
@@ -3,9 +3,15 @@
 # Possession of a copy of this file grants no permission or license
 # to use, modify, or create derivate works.
 # Please visit http://www.peerworks.org/contact for further information.
-class ProtectedItem < ActiveRecord::Base
-  belongs_to :protector, :counter_cache => true
-  belongs_to :feed_item
-  validates_uniqueness_of :feed_item_id, :scope => :protector_id
-  validates_presence_of   :protector_id, :feed_item_id
+
+
+class RemoveItemProtection < ActiveRecord::Migration
+  def self.up
+    drop_table :protected_items
+    drop_table :protectors    
+  end
+
+  def self.down
+    raise IrreversibleMigration
+  end
 end
