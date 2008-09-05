3e58f5937719aea1be1587706fddfd712692c271
Sean Geoghegan <seangeo@gmail.com>
2008-09-05 13:00:19 +0930

    Added build_from_feed_item method to atom document.

--- app/models/feed_item_atom_document.rb
+++ app/models/feed_item_atom_document.rb
@@ -7,4 +7,30 @@
 
 class FeedItemAtomDocument < ActiveRecord::Base
   belongs_to :feed_item
+  
+  def self.build_from_feed_item(feed_item_id, item, options = {})    
+    atom_entry = Atom::Entry.new do |entry|
+      entry.title = item.title
+      entry.id = "urn:peerworks.org:entry##{feed_item_id}"
+      entry.updated = item.time
+      entry.authors << Atom::Person.new(:name => item.author.name, :email => item.author.email) if item.author && item.author.name
+      entry.links << Atom::Link.new(:rel => 'self', 
+                                    :href => "#{options[:base]}/feed_items/#{feed_item_id}.atom")
+      entry.links << Atom::Link.new(:rel => 'alternate', :href => item.link)
+      entry.links << Atom::Link.new(:rel => 'http://peerworks.org/rel/spider', 
+                                    :href => "#{options[:base]}/feed_items/#{feed_item_id}/spider")     
+      # Content could be non-utf8 or contain non-printable characters due to a FeedTools pre 0.2.29 bug.
+      # LibXML chokes on this so try and fix it.
+      if item.content
+        begin
+          entry.content = Atom::Content::Html.new(Iconv.iconv('utf-8', 'utf-8', item.content).first.tr("\000-\011", ""))
+        rescue Iconv::IllegalSequence
+          # LATIN1 is the most likely, try that or fail
+          entry.content = Atom::Content::Html.new(Iconv.iconv('utf-8', 'LATIN1', item.content).first.tr("\000-\011", ""))
+        end
+      end
+    end
+    
+    new(:atom_document => atom_entry.to_xml, :feed_item_id => feed_item_id)
+  end
 end
--- spec/models/feed_item_atom_document_spec.rb
+++ spec/models/feed_item_atom_document_spec.rb
@@ -15,4 +15,92 @@ describe FeedItemAtomDocument do
   it "should be valid" do
     @feed_item_atom_document.should be_valid
   end
+  
+  describe '.create_from_feed_tools' do
+    before(:each) do
+      feed_url = 'file:/' + File.join(File.expand_path(RAILS_ROOT), 'spec', 'fixtures', 'slashdot.rss')
+      feed = FeedTools::Feed.open(feed_url)      
+      @item = feed.items.first
+      @doc = FeedItemAtomDocument.build_from_feed_item(1234, @item, :base => 'http://collector.mindloom.org')
+      @entry = Atom::Entry.load_entry(@doc.atom_document)
+    end
+    
+    it "should set the feed_item_id" do
+      @doc.feed_item_id.should == 1234
+    end
+    
+    it "should return an Atom:Entry" do
+      @entry.should be_an_instance_of(Atom::Entry)
+    end
+    
+    it "should have the title" do
+      @entry.title.should == @item.title
+    end
+    
+    it "should have the id" do
+      @entry.id.should == "urn:peerworks.org:entry#1234"
+    end
+    
+    it "should have the updated date" do
+      @entry.updated.should == @item.time
+    end
+    
+    it "should have the author's name" do
+      @entry.authors.first.name.should == @item.author.name
+    end
+    
+    it "should have the content" do
+      @entry.content.should == @item.content.gsub("\n", ' ')
+    end
+    
+    it "should have the content type" do
+      @entry.content.type.should == 'html'
+    end
+    
+    it "should have the alternate link pointing to link" do
+      @entry.alternate.href.should == @item.link
+    end
+
+    it "should have a spider link" do
+      @entry.links.select {|l| l.rel == 'http://peerworks.org/rel/spider'}.should_not be_empty
+    end
+    
+    it "should have the spider link pointing to http://base/feed_items/:id/spider" do
+      @entry.links.select {|l| l.rel == 'http://peerworks.org/rel/spider'}.first.href.should == 
+          "http://collector.mindloom.org/feed_items/1234/spider"
+    end
+    
+    describe 'without author' do
+      before(:each) do        
+        @item.author = nil
+        doc = FeedItemAtomDocument.build_from_feed_item("1234", @item, :base => 'http://blah')
+        @entry = Atom::Entry.load_entry(doc.atom_document)
+      end
+      
+      it "should be have no authors" do
+        @entry.should have(0).authors
+      end
+    end
+  
+    describe 'create_from_feed_tools with non-utf-8 content' do
+      before(:each) do
+        @item.content = "This is not utf-8 because of this character: \225"
+        doc = FeedItemAtomDocument.build_from_feed_item("1234", @item, :base => 'http://blah')
+        @entry = Atom::Entry.load_entry(doc.atom_document)
+      end
+    
+      it "should re-encode the content if it can" do
+        @entry.content.to_s.should == Iconv.iconv('utf-8', 'LATIN1', "This is not utf-8 because of this character: \225").first
+      end
+    end
+  
+    describe "create_from_feed_tools with non-printable characters" do
+      it "should remove them" do
+        @item.content = "This has a non\004-printable character"
+        doc = FeedItemAtomDocument.build_from_feed_item("1234", @item, :base => 'http://blah')
+        @entry = Atom::Entry.load_entry(doc.atom_document)
+        @entry.content.should == "This has a non-printable character"
+      end
+    end
+  end
 end
