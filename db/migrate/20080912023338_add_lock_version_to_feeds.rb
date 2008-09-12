b857a26fcf84da3271eb9334bfe4908f3c9719e6
Sean Geoghegan <seangeo@gmail.com>
2008-09-12 12:05:40 +0930

    Added lock version to feeds.

---
+++ db/migrate/20080912023338_add_lock_version_to_feeds.rb
@@ -0,0 +1,16 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
+
+
+class AddLockVersionToFeeds < ActiveRecord::Migration
+  def self.up
+    add_column :feeds, :lock_version, :integer
+  end
+
+  def self.down
+    remove_column :feeds, :lock_version
+  end
+end
