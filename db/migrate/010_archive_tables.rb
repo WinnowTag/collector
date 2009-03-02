710004b77e995802ef03417eaf90f5e4282103bd
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-03-02 17:08:50 -0500

    adding missing feed_xml_datas. Moving setup to the initial schema file

--- db/migrate/010_archive_tables.rb
+++ db/migrate/010_archive_tables.rb
@@ -5,68 +5,6 @@
 # Please visit http://www.peerworks.org/contact for further information.
 class ArchiveTables < ActiveRecord::Migration
   def self.up
-    # Creating these tables so migration run from start to finish.
-    create_table :feed_items do |t|
-      t.integer :feed_id, :default => 0
-      t.integer :collection_job_id
-      t.string :title
-      t.string :link
-      t.datetime :time
-      t.string :unique_id
-      t.string :atom_md5
-      t.integer :content_length, :default => 0
-      t.datetime :created_on
-      t.string :sort_title
-    end
-    add_index :feed_items, :feed_id
-    add_index :feed_items, :collection_job_id
-    add_index :feed_items, :time
-    add_index :feed_items, :unique_id
-    add_index :feed_items, :content_length
-    add_index :feed_items, :sort_title
-
-    create_table :feeds do |t|
-      t.string :url
-      t.string :title
-      t.string :sort_title
-      t.string :link
-      t.boolean :active, :default => true
-      t.integer :duplicate_id
-      t.integer :feed_items_count, :default => 0
-      t.integer :collection_errors_count, :default => 0
-      t.datetime :updated_on
-      t.datetime :created_on
-      t.integer :lock_version, :default => 0
-      t.string :uri
-      t.text :last_http_headers
-      t.boolean :is_duplicate
-    end
-    add_index :feeds, :uri, :unqiue => true
-    add_index :feeds, :sort_title
-
-    create_table "feed_item_xml_data" do |t|
-      t.text     "xml_data",   :limit => 2147483647
-      t.datetime "created_on"
-    end
-
-    create_table "feed_item_contents" do |t|
-      t.integer  "feed_item_id",    :limit => 11
-      t.text     "title"
-      t.string   "link"
-      t.string   "author"
-      t.text     "description",     :limit => 2147483647
-      t.datetime "created_on"
-      t.text     "encoded_content"
-    end
-    execute "ALTER TABLE feed_item_contents ENGINE=MYISAM;"
-    add_index :feed_item_contents, :feed_item_id, :name => "feed_item_contents_feed_item_id_index"
-    execute "ALTER TABLE feed_item_contents ADD FULLTEXT fti_feed_item_contents(title, author, description);"
-    
-    create_table "schema_info", :id => false do |t|
-      t.integer "version"
-    end
-    execute "alter table schema_info ENGINE=MYISAM;"
-    
     execute "create table feed_items_archives like feed_items;"
     add_column :feed_items_archives, :position, :integer
     remove_column :feed_items_archives, :title
