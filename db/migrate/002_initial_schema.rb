790a915519b87926eb541bfa68dddb4d49550313
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-03-02 17:15:37 -0500

    fix old name of column

--- db/migrate/002_initial_schema.rb
+++ db/migrate/002_initial_schema.rb
@@ -23,7 +23,7 @@ class InitialSchema < ActiveRecord::Migration
     # Creating these tables so migration run from start to finish.
     create_table :feed_items do |t|
       t.integer :feed_id, :default => 0
-      t.integer :collection_job_id
+      t.integer :collection_id
       t.string :title
       t.string :link
       t.datetime :time
@@ -35,7 +35,7 @@ class InitialSchema < ActiveRecord::Migration
       t.string :time_source
     end
     add_index :feed_items, :feed_id
-    add_index :feed_items, :collection_job_id
+    add_index :feed_items, :collection_id
     add_index :feed_items, :time
     add_index :feed_items, :unique_id
     add_index :feed_items, :content_length
