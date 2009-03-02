db73af9fe52ef63a749caf54357891fc9fa205d7
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-03-02 16:59:38 -0500

    Removing another missing table

--- db/migrate/035_remove_winnow_feed_schema_info.rb
+++ db/migrate/035_remove_winnow_feed_schema_info.rb
@@ -5,7 +5,7 @@
 # Please visit http://www.peerworks.org/contact for further information.
 class RemoveWinnowFeedSchemaInfo < ActiveRecord::Migration
   def self.up
-    drop_table(:winnow_feed_schema_info)
+    # drop_table(:winnow_feed_schema_info)
   end
 
   def self.down
