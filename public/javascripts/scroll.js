b464ae8aa7a20267d7a906185c36e9c96bbbb786
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-10-21 23:01:46 -0400

    ticket #1112 - Correct misspelling of "derivative" in file copyright headers

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2008 The Kaphan Foundation
 //
 // Possession of a copy of this file grants no permission or license
-// to use, modify, or create derivate works.
+// to use, modify, or create derivative works.
 // Please visit http://www.peerworks.org/contact for further information.
 Effect.ScrollToInDiv = Class.create(Effect.Base, {
   initialize: function(container, element) {
